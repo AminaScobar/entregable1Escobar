dcd35121147affc0266d8050d8f9cf963f9819f0
Melqui Escobar <melquiescobar@MacBook-Pro-de-Melqui.local>
2024-09-27 15:14:59 -0600

    nuevo cambio

--- js/javaScript.js
+++ js/javaScript.js
@@ -4,7 +4,7 @@ let nombreCliente;
 
 function qIdiomaHablas () {
 
-   let idioma = prompt("Bienvenido al cotizador automático del taller automotriz Gonzalez, si hablas español, digita ES. / Welcome to the Gonzalez auto shop automatic quote, if you speak English, type EN.").toLowerCase()
+   let idioma = prompt("Bienvenido al cotizador automático del taller automotriz Gonzalez. \nSi hablas español, digita ES. \n\nWelcome to the Gonzalez auto shop automatic quote. \nIf you speak English, type EN.").toLowerCase()
    if (idioma === ingles) {
       alert("At this time, we cannot offer you our automatic quote services, but you can write us your concerns to the following email: gtaller@gmail.com");
       return;
@@ -25,7 +25,7 @@ let ofrecerProductoUno = "hola " + nombreCliente + " ingresa el código segun el
 let ofrecerProductoOtros = nombreCliente + " ¿quieres cotizar otro producto? ingresa el código segun el servicio a cotizar: \nCambio de aceite motor, código: CAM.\nCambio de aceite transmisión, código: CAT.\nCambio de frenos, código: CF.\ncambio de bujias, código: CB.\nningun otro producto, código: NO.";
 let preguntas = [ ofrecerProductoUno, ofrecerProductoOtros, ofrecerProductoOtros, ofrecerProductoOtros ];
 
-const codigoProductos = ["cam", "cat", "cf", "cf", "no"]
+const codigoProductos = ["cam", "cat", "cf", "cb", "no"]
 const preciosProductos = [40, 100, 50, 60, 0]
 let precioProductoUno = 0;
 let precioProductoDos = 0;
